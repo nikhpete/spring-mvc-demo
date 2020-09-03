b53814f2c6074b08593493289d7e1f71dca156a6
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-03 11:53:42 +0530

    Controller level request mapping

--- src/com/nick/springdemo/mvc/HelloWorldController.java
+++ src/com/nick/springdemo/mvc/HelloWorldController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldCOntroller {
+@RequestMapping("/hello")
+public class HelloWorldController {
 
 	@RequestMapping("/showForm")
 	public String showForm() {
